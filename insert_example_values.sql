c2f476f436e4112903576bb88afc4bcb36d71ecd
Jonas Müller <jonas.mueller.contact@gmail.com>
2020-06-10 15:58:02 +0200

    Kommentare fuer Struktur

--- insert_example_values.sql
+++ insert_example_values.sql
@@ -1,5 +1,23 @@
+/**
+  >===========================================================================================================<
+  >================================================Felix======================================================<
+*/
+
+-- Lebensmittel
+
 insert into LEBENSMITTEL VALUES ('Salat');
 insert into LEBENSMITTEL VALUES ('Mehl');
 insert into LEBENSMITTEL VALUES ('Eier');
 insert into LEBENSMITTEL VALUES ('Wasser');
 insert into LEBENSMITTEL VALUES ('Butter');
+
+-- Einkaufspreise
+
+/**
+  >===========================================================================================================<
+  >================================================Jonas======================================================<
+*/
+
+-- Abrechnungen
+
+-- Tische
